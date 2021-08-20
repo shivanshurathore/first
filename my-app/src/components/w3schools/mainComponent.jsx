9665a03618a06cf147994e006be9ea4324bbdd51
Shivanshu Rathore <shivanshu@daffodilsw.com>
2021-08-20 13:00:32 +0530

    commit my-app 2

---
+++ my-app/src/components/w3schools/mainComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MainComonent extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div className="container"></div>
+        );
+    }
+}
+
+export default MainComonent;
