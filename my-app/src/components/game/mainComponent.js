5f9fb98c52c259804bd61046037157eb44860a0c
Shivanshu Rathore <shivanshu@daffodilsw.com>
2021-09-01 12:59:56 +0530

    game final_1_Sep_2021

--- my-app/src/components/game/mainComponent.js
+++ my-app/src/components/game/mainComponent.js
@@ -28,6 +28,47 @@ class MainComponent extends Component {
       { image: dog, open: false },
       { image: monkey, open: false },
     ],
+    open1: "",
+    open2: "",
+  };
+
+  showImg = (index) => {
+    let s1 = { ...this.state };
+    s1.dataArr[index].open = true;
+    s1.open1
+      ? (s1.open2 = s1.dataArr[index].image)
+      : (s1.open1 = s1.dataArr[index].image);
+    if (s1.open1 && s1.open2) setTimeout(() => this.doCellsMatching(), 1000);
+    this.setState(s1);
+  };
+
+  doCellsMatching = () => {
+    let s1 = { ...this.state };
+    if (s1.open1 === s1.open2) {
+      s1.open1 = "";
+      s1.open2 = "";
+    }
+    if (s1.open1 !== s1.open2) {
+      let index1 = s1.dataArr.findIndex(
+        (val) => val.image === s1.open1 && val.open === true
+      );
+      s1.dataArr[index1].open = false;
+      s1.open1 = "";
+      let index2 = s1.dataArr.findIndex(
+        (val) => val.image === s1.open2 && val.open === true
+      );
+      s1.dataArr[index2].open = false;
+      s1.open2 = "";
+    }
+    this.setState(s1);
+  };
+
+  resetGame = () => {
+    let s1 = { ...this.state };
+    for (let i = 0; i < s1.dataArr.length; i++) {
+      s1.dataArr[i].open = false;
+    }
+    this.setState(s1);
   };
 
   render() {
@@ -36,14 +77,20 @@ class MainComponent extends Component {
     return (
       <div className="main">
         <div className="btnDiv">
-          {dataArr.map((item) => (
-            <button className="btn1" key={item}>
+          {dataArr.map((item, index) => (
+            <button
+              className="btn1"
+              key={item}
+              onClick={() => this.showImg(index)}
+            >
               {/* <img src={item.image} alt="animals" /> */}
               {item.open ? <img src={item.image} alt="animals" /> : ""}
             </button>
           ))}
         </div>
-        <button className="resetBtn">Reset Game</button>
+        <button className="resetBtn" onClick={this.resetGame}>
+          Reset Game
+        </button>
       </div>
     );
   }
