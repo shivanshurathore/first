e707172225ba18d1832a60ee9ceae0c9597e00b3
shivanshu <shivanshu@daffodilsw.com>
2021-09-06 10:16:02 +0530

    commit

--- server/serverForLoginSignup.js
+++ server/serverForLoginSignup.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const port = 2410;
-app.listen(`listining to port ${port}`);
+app.listen(console.log(`listining to port ${port}`), port);
 
 app.get("/createCollection", function (req, res) {
   MongoClient.connect(url, function (err, db) {
